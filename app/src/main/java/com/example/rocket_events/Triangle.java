65d307186b90e8184718ec066fdc9696d29a63d4
27100240 <alizakh004@gmail.com>
2026-02-17 14:53:21 +0500

    Added Triangle class extending Shape

---
+++ app/src/main/java/com/example/rocket_events/Triangle.java
@@ -0,0 +1,14 @@
+package com.example.rocket_events;
+
+public class Triangle extends Shape {
+
+    int base;
+    int height;
+
+    public Triangle(int x, int y, int base, int height) {
+        this.x = x;
+        this.y = y;
+        this.base = base;
+        this.height = height;
+    }
+}
