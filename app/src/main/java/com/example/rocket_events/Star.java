081bee0b4ebe1efdf7cc200622aee8bbcda9878f
27100024 <27100024@lums.edu.pk>
2026-02-17 17:34:54 +0500

    Create Star.java

---
+++ app/src/main/java/com/example/rocket_events/Star.java
@@ -0,0 +1,10 @@
+package com.example.rocket_events;
+
+public class Star extends Shape {
+    private int points;
+
+    public Star(int x, int y, int points) {
+        super(x, y);
+        this.points = points;
+    }
+}
