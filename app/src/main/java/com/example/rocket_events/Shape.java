02c9adcdf4462386791c66758a59486da731a81a
Mahad Bhatti <mahad.bhatti04@gmail.com>
2026-02-17 15:19:23 +0500

    Add color field locally

--- app/src/main/java/com/example/rocket_events/Shape.java
+++ app/src/main/java/com/example/rocket_events/Shape.java
@@ -3,7 +3,7 @@ package com.example.rocket_events;
 public abstract class Shape {
     int x;
     int y;
-
+    String color= "blue";
     public Shape(int x, int y) {
         this.x = x;
         this.y = y;
